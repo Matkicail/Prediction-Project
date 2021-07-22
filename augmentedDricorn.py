daf0f53e89eea2fdaea5d1bff1a23e335b364ec1
Matkicail <61425800+Matkicail@users.noreply.github.com>
2021-07-22 08:35:14 +0200

    Removed unnecessary imports

--- augmentedDricorn.py
+++ augmentedDricorn.py
@@ -1,7 +1,5 @@
 from datetime import date
-from os import read
 import numpy as np
-from numpy.core.records import array
 import pandas as pd
 from matplotlib import pyplot as plt
 from stockMarketReader import readDataSet
--- stockMarketReader.py
+++ stockMarketReader.py
@@ -1,9 +1,6 @@
 from datetime import date
-from os import read
 import numpy as np
-from numpy.core.records import array
 import pandas as pd
-from matplotlib import pyplot as plt
 
 def findBestStartDate(data):
     """
