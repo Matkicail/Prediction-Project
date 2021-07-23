fd8d608609227b3402b3ca36ece90463493bfc41
Matkicail <61425800+Matkicail@users.noreply.github.com>
2021-07-23 15:58:13 +0200

    Create CRP.py
    Added new baseline file

---
+++ CRP.py
@@ -0,0 +1,10 @@
+from datetime import date
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from stockMarketReader import readDataSet
+# ######################################## #
+# Implementing the CRP Portfolio Benchmark #
+# ######################################## #
+
+data = readDataSet()
