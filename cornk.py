07c3538de242bc1e1a131ccd50110245b2dcbe6e
Matkicail <61425800+Matkicail@users.noreply.github.com>
2021-08-03 11:16:44 +0200

    Update cornk.py
    Not functioning still but getting there.
    Currently making price relative vector history generator.

--- cornk.py
+++ cornk.py
@@ -22,10 +22,18 @@ class Expert:
         self.corrThresh = corrThresh
         self.corrSimSet = None
         self.numStocks = numStocks
-        self.wealthAchieved = 0
+        self.weight = 0
+        # initial wealth as based on page 12 note
+        self.wealthAchieved = 1
+        self.currPort = None
         self.portHistory = np.empty((numStocks, numDays))
         
-    
+    def updateWeight(self, numExperts):
+        """
+        Update this agent's weight to be 1/numExperts as it is part of top-numExperts (K)
+        """
+        self.weight = 1 / numExperts
+
     def assignCorrSet(self, corrSet):
         """
         A function that allows us to add a correlation similar set to this specific expert - should it be needed.
@@ -41,13 +49,6 @@ class Expert:
         for i in range(self.numStocks):
             self.portHistory[day][i] = portfolio[i]
     
-    
-    def setInitialAgentWealth(self, startWealth):
-        """
-        Function that sets an agent's init wealth to a specified amount.
-        """
-        self.wealthAchieved = startWealth
-
     def increaseWealth(self, portfolio, priceVector):
         """
         Function that given a portfolio and a price relative vector will increase the agent's wealth using it.
@@ -66,28 +67,42 @@ def getUniformPort():
     stocks = np.ones((numStocks))
     return stocks / numStocks
 
-def expertLearn(window, corrThresh, day):
+def expertLearn(window, corrThresh, day, data):
     """
     Preform algorithm 1 from CORN paper.
     This algorithm is the expert learning procedure.
     Given an index date (day), the window (a specified window size), the histMarketWind (t-1 to 1) and the corrThresh which is rho our correlation coeff threshold.
     """
-    corrSimSet = np.array()
+    corrSimSet = np.array(())
     if day <= window + 1:
-        return getUniformPort
+        #works
+        return getUniformPort()
     else:
+        #check out
         for i in range(window + 1,day): #just check that this works otherwise change it to t
+            # print("Test 1")
             markWindI = marketWindow(i-window, i-1, dates, data)
+            # print("Test 2")
             markWindT = marketWindow(day - window, day - 1, dates, data)
+            # print("Test 3")
             # check at some point to ensure that this captures the standard deviation for the whole window (i.e output not something weird)
             # flattened just to ensure that this does happen
-            if np.std(np.flatten(markWindI)) == 0 or np.std(np.flatten(markWindT)) == 0:
-                corrThresh = 0
+            pearsonCor = -1
+            if np.std(markWindI.flatten()) == 0 or np.std(markWindT.flatten()) == 0:
+                print("Recognised a 0 standard deviation in a market window")
+                pearsonCor = 0
+            # print("Test 4")
             # may need to change this to the exact calculation they use in the formula
-            if np.corrcoef(markWindI, marketWindow) >= corrThresh:
+            if pearsonCor != 0:
+                pearsonCor = np.cov(markWindI, markWindT) / (np.std(markWindI.flatten()) * np.std(markWindT.flatten()) )
+                # print("Test 5")
+            elif  pearsonCor >= corrThresh:
+                print("Appended a set")
                 # append this to our set i.e add the index
                 corrSimSet = np.append(corrSimSet,i)
+            # print("Test 6")
     if len(corrSimSet) == 0:
+        # print("Empty set")
         return getUniformPort()
     else:
         # Search for the optimal portfolio
@@ -109,12 +124,14 @@ def expertLearn(window, corrThresh, day):
                     tempRelative = day
                     port = np.zeros((numStocks))
                     port[np.argmax(day,axis=0)] = 1
+        print("WAS ABLE TO FIND AN OPTIAML PORT")
         return port
         
 def dayReturn(day, dates, data):
     """
     Given a day, the dates and a dataframe.
     Get stock market data for the given day - organise it vertically.
+    SOME ISSUE EXISTS HERE SO DO NOT USE THIS
     """
     if day != 0:
         try:
@@ -143,6 +160,21 @@ def getDatesVec(data):
     tickerDates = data[data['Ticker'] == tick]
     tickerDates = data.Date.to_numpy()
     return tickerDates
+
+def generateHistoricalMarket(data, dates, numStocks):
+    """
+    
+    """
+    relatives = np.array((numStocks, len(dates)))
+    initalDay = np.ones((numStocks,1))
+    relatives[:,0] = initalDay
+    for i in range(1,len(dates)):
+        marketToday = data[data['Date'] == dates[i]]
+        marketYesterday = data[data['Date'] == dates[i-1]]
+        change = marketToday.Close.to_numpy()/marketYesterday.Close.to_numpy()
+        change = change.reshape(numStocks,1)
+        relatives[:,i] = change
+    return relatives
 
 def marketWindow(startDate, endDate, dates, data):
     """
@@ -179,12 +211,75 @@ def calcReturns(portfolios, dates, data, initialCapital = 1):
             val += portfolios[i][j] * day[j]
         returns = np.append(returns, val)
     return initialCapital * returns
+
+def initExperts(windowSize, numStocks, P):
+    """
+    Initialise all the experts. 
+    Given a windowSize(max) assign some to each.
+    For a number of stocks that are given by the tickers.
+    For P where we will figure out our correlation.
+    """
+
+    # init W*P experts
+    experts = np.array((), dtype=object)
+    for i in range(1,windowSize):
+        for j in range(P):
+            index = (windowSize-1)*i + j
+            # __init__(self, windowSize, corrThresh, numStocks, numDays):
+            expert = Expert(i,j/P, numStocks, len(dates))
+            experts = np.append(experts, expert)
+    return experts
+
+def printExperts(experts, windowSize, P):
+    """
+    Function to print the experts.
+    Pay attention to the indexing, since a 0 window does not make sense it feels
+    """
+    for i in range(P):
+        for j in range(0,windowSize-1):
+            print("Expert at " + str(i*(windowSize-1) + j) +" with characteristics:"+str(experts[i*(windowSize-1) +j].windowSize) + "," + str(experts[i*(windowSize-1) +j].corrThresh))
+
+def runCorn(dates, data, windowSize, P):
+    """
+    Run the CORN-K algorithm on the data set
+    """
+    # create experts which a 1D array
+    experts = initExperts(windowSize,numStocks,P)
+    experts = experts.reshape(windowSize-1,P)
+    # going downwards window size increases, going rightwards the corrThresh increases
+    totError = 0
+    windowError = np.zeros((windowSize-1))
+    corrThreshError = np.zeros((P))
+    # starting from first day to the final day
+    for i in range(len(dates)):
+        # for each window size as based on the experts which is of length windowSize - 1
+        for w in range(windowSize - 1):
+            # for each corrThresh in the width which is P wide
+            for p in range(P):
+                # Apply the corn expert learning algorithm on this expert using its parameters
+                try:
+                    experts[w][p].currPort = expertLearn(experts[w][p].windowSize, experts[w][p].corrThresh, i, data)
+                except:
+                    
+                    print("Expert character: " + str(experts[w][p].windowSize) + " ," + str(experts[w][p].corrThresh))
+                    print("Error at day: " + str(i) + ", window: " + str(w+1) + ", corrThresh: " + str(p))
+                    if i == 4:
+                        return
+    # combine our experts
+
+
 data = readDataSet()
 dates = getDatesVec(data)
 tempStartFind = data[data['Date'] == dates[0]]
 tempTickersFind = np.unique(tempStartFind.Ticker.to_numpy())
 numStocks = len(tempTickersFind)
 today = dayReturn(1,dates,data)
-market = marketWindow(1007,1012,dates,data, numStocks)
+market = marketWindow(1007,1012,dates,data)
 print(market)
-
+windowSize = 3
+P = 3
+expertLearn(windowSize, 0, 2, data)
+experts = initExperts(windowSize,numStocks,P)
+# printExperts(experts,windowSize,P)
+# runCorn(dates,data,windowSize,P)
+print(generateHistoricalMarket(data, dates, numStocks))
