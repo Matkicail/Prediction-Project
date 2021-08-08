7e0324faf2285752b7262dbdf4babeff1826b461
Matkicail <61425800+Matkicail@users.noreply.github.com>
2021-08-08 16:55:35 +0200

    Updated Showcase
    Now includes CORN-K returns from text using the STANDARDISED parameters for CORN-K
    i.e window = 5, P = 10, K = 5

--- showcase.py
+++ showcase.py
@@ -2,25 +2,29 @@ from datetime import date
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+import math
 from stockMarketReader import readDataSet
 from bestStock import bestStockStrategy
 from ubah import ubah
 from CRP import UCRP
 
+
 data = readDataSet()
 # bestStock = np.log(bestStockStrategy(data))
-# ubahPort = np.log(ubah(data))
-# crpPort = np.log(UCRP(data))
+ubahPort = np.log(ubah(data))
+crpPort = np.log(UCRP(data))
 
 # bestStock = bestStockStrategy(data)
-ubahPort = ubah(data)
-crpPort = UCRP(data)
-
+# ubahPort = ubah(data)
+# crpPort = UCRP(data)
+cornReturns = np.log(np.loadtxt("BOV800DAYCORNRETURNS.txt"))
+numDays = len(cornReturns)
 plt.title("Comparison of Strategies")
 plt.xlabel("Number of Days")
 plt.ylabel("Total Return")
 # plt.plot(bestStock, label="Best Stock")
-plt.plot(ubahPort, label="UBAH")
-plt.plot(crpPort, label="CRP")
+plt.plot(ubahPort[0:numDays], label="UBAH")
+plt.plot(crpPort[0:numDays], label="CRP")
+plt.plot(cornReturns, label="CORN")
 plt.legend()
 plt.show()
