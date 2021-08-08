d5b1ab1fcc429f9e3267b3dcaf8eb43dd406bec8
Matkicail <61425800+Matkicail@users.noreply.github.com>
2021-08-08 16:55:02 +0200

    Working CORN-K

--- cornk.py
+++ cornk.py
@@ -398,7 +398,7 @@ def runCorn(dates, data, windowSize, P):
 
         # if val == 0:
         #     print("VALUE IS 0 AT DAY" + str(i))
-        if i == 100:
+        if i == 800:
             return returns
     return returns
 data = readDataSet()
@@ -441,6 +441,6 @@ K = 5
 wealth = runCorn(dates,dataset,windowSize,P)
 print("Minimum value in wealth array: " + str(wealth.min()))
 print("Maximum value in wealth array: " + str(wealth.max()))
-np.savetxt("NASCORNRETURNS.txt",wealth)
-plt.plot(wealth)
-plt.show()
+np.savetxt("TEMPSERVERCORNRETURNS.txt",wealth)
+# plt.plot(wealth)
+# plt.show()
