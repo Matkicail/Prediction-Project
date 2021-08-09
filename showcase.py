df6ff8a9f9be1497f531c7ac190575651ce4dcac
Matkicail <61425800+Matkicail@users.noreply.github.com>
2021-08-09 23:28:26 +0200

    More DataSets and showcase extended
    EUR data set needs attention - issues exist

--- showcase.py
+++ showcase.py
@@ -7,26 +7,46 @@ from stockMarketReader import readDataSet
 from bestStock import bestStockStrategy
 from ubah import ubah
 from CRP import UCRP
-
+from scipy import stats
 
 data = readDataSet()
+name = "BOV500"
 # bestStock = np.log(bestStockStrategy(data))
 # ubahPort = np.log(ubah(data))
 # crpPort = np.log(UCRP(data))
-cornReturns = np.log(np.loadtxt("JSE500DAYCORNRETURNS.txt"))
+# bestStock = np.log(bestStockStrategy(data))
+# cornReturns = np.log(np.loadtxt("./Data Sets/CORNK/" + name + "DAYCORNRETURNS.txt"))
+# racornReturns = np.log(np.loadtxt("./Data Sets/RACORNK/" + name + "DAYRACORNRETURNS.txt"))
 
 # bestStock = bestStockStrategy(data)
+# ubahPort = ubah(data)
+# crpPort = UCRP(data)
+# cornReturns = np.loadtxt("./Data Sets/CORNK/" + name + "DAYCORNRETURNS.txt")
+# racornReturns = np.loadtxt("./Data Sets/RACORNK/" + name + "DAYRACORNRETURNS.txt")
+
+# box-cox transform showcase
+bestStock = bestStockStrategy(data)
 ubahPort = ubah(data)
 crpPort = UCRP(data)
-cornReturns = np.loadtxt("JSE500DAYCORNRETURNS.txt")
+cornReturns = np.loadtxt("./Data Sets/CORNK/" + name + "DAYCORNRETURNS.txt")
+racornReturns = np.loadtxt("./Data Sets/RACORNK/" + name + "DAYRACORNRETURNS.txt")
+numDays = len(cornReturns)
+bestStock, bestStock_lambda = stats.boxcox(bestStockStrategy(data[0:numDays]))
+ubahPort, ubah_lambda = stats.boxcox(ubahPort[0:numDays])
+crpPortPort, crpPort_lambda = stats.boxcox(crpPort[0:numDays])
+cornReturns, cornReturns_lambda = stats.boxcox(cornReturns)
+racornReturns, racornReturns_lambda = stats.boxcox(racornReturns)
 
 numDays = len(cornReturns)
+if len(racornReturns) < numDays:
+    cornReturns = cornReturns[0:len(racornReturns)]
 plt.title("Comparison of Strategies")
 plt.xlabel("Number of Days")
 plt.ylabel("Total Return")
-# plt.plot(bestStock[0:numDays], label="Best Stock")
+plt.plot(bestStock[0:numDays], label="Best Stock")
 plt.plot(ubahPort[0:numDays], label="UBAH")
 plt.plot(crpPort[0:numDays], label="CRP")
 plt.plot(cornReturns, label="CORN")
+plt.plot(racornReturns, label="RACORN")
 plt.legend()
 plt.show()
