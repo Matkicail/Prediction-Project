1b01d6c536f027c2eecf17ce4f790f307377dff4
Matkicail <61425800+Matkicail@users.noreply.github.com>
2021-08-18 12:25:24 +0200

    Main file changes
    Issues preventing a proper commit

--- bestStock.py
+++ bestStock.py
@@ -4,13 +4,36 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from stockMarketReader import readDataSet
 
-def bestStockStrategy(data):
+def getDatesVec(data):
+    """
+    Get the vector of dates that spans a given stock market data set - specifically done for CORN algorithm but not exclusive to it
+    note that this requires the pandas dataframe of data
+    NOTE pandas dataframe for data
+    """
+    startDate = data.Date.min()
+    startDate = data[data['Date'] == startDate]
+    startDate = startDate.Ticker.to_numpy()
+    tick = np.unique(startDate)[0]
+    tickerDates = data[data['Ticker'] == tick]
+    tickerDates = np.unique(data.Date.to_numpy())
+    return tickerDates
+
+def bestStockStrategy(data, startAt, stopAt):
     """
     Given a specific stock exchange, this will find the best stock, record the returns of the best stock daily and display it as a plot.
     The returns are however available as they will be returned from this function.
     """
+
     startDate = data.Date.min()
-    endDate = data.Date.max()
+    
+    dates = getDatesVec(data)
+    startDate = dates[startAt]
+    endDate = dates[stopAt]
+    
+    data = data[data["Date" ] >= startDate]
+    # print(data.head(10))
+    data = data[data["Date" ] <= endDate]
+    # print(data.head(10))
     startPrices = data[data['Date'] == startDate]
     startOfMarket = startPrices.copy()
     startPrices = startPrices.Close.to_numpy()
@@ -23,8 +46,14 @@ def bestStockStrategy(data):
     # Need to grab the right stock at the start date, so two parts to do here
     returns = np.array(())
     # Here we are grabbing the data of the ticker that achieved maximal returns
+    
     marketData = data[data['Ticker'] == tickers[indexMax]]
+    # marketData = marketData[data['Date'] >= startDate]
+    # marketData = marketData[data['Date'] < endDate]
+    print("HERE")
+    print(marketData.head(10))
     dates = marketData.Date.to_numpy()
+    print(dates)
     for i in dates:
         currDay = marketData[marketData['Date'] == i]
         returns = np.append(returns, initProp * currDay.Close)
@@ -33,7 +62,7 @@ def bestStockStrategy(data):
     # plt.title("Total Return of " + tickers[indexMax])    
     # plt.plot(returns)
     # plt.show()
-    return returns
+    return returns, tickers[indexMax]
 
 # data = readDataSet()
 # bestStockStrategy(data)
--- showcase.py
+++ showcase.py
@@ -10,24 +10,28 @@ from CRP import UCRP
 from scipy import stats
 
 data = readDataSet()
-name = "JSE600"
-# bestStock = np.log(bestStockStrategy(data))
+name = "BOV600"
+startDate = 2500
+endDate = 2650
+# bestStock, stockName = np.log(bestStockStrategy(data))
 # ubahPort = np.log(ubah(data))
 # crpPort = np.log(UCRP(data))
 # # bestStock = np.log(bestStockStrategy(data))
 # cornReturns = np.log(np.loadtxt("./Data Sets/CORNK/" + name + "DAYCORNRETURNS.txt"))
 # racornReturns = np.log(np.loadtxt("./Data Sets/RACORNK/" + name + "DAYRACORNRETURNS.txt"))
 # dricornReturns = np.log(np.loadtxt("./Data Sets/DRICORNK/" + name + "DAYDRICORNRETURNS.txt"))
+# weirdkReturns= np.load(np.load("./Data Sets/WEIRDK/" + name + ".txt"))
 
-# bestStock = bestStockStrategy(data)
-ubahPort = ubah(data)
-crpPort = UCRP(data)
-cornReturns = np.loadtxt("./Data Sets/CORNK/" + name + "DAYCORNRETURNS.txt")
-racornReturns = np.loadtxt("./Data Sets/RACORNK/" + name + "DAYRACORNRETURNS.txt")
-dricornReturns = np.loadtxt("./Data Sets/DRICORNK/" + name + "DAYDRICORNRETURNS.txt")
+bestStock, stockName = bestStockStrategy(data, startDate, endDate)
+ubahPort = ubah(data, startDate, endDate)
+crpPort = UCRP(data, startDate, endDate)
+# cornReturns = np.loadtxt("./Data Sets/CORNK/" + name + "DAYCORNRETURNS.txt")
+# racornReturns = np.loadtxt("./Data Sets/RACORNK/" + name + "DAYRACORNRETURNS.txt")
+# dricornReturns = np.loadtxt("./Data Sets/DRICORNK/" + name + "DAYDRICORNRETURNS.txt")
+# weirdkReturns= np.loadtxt("./Data Sets/WEIRDK/" + name + ".txt")
 
 # box-cox transform showcase
-# bestStock = bestStockStrategy(data)
+# bestStock, stockName = bestStockStrategy(data)
 # ubahPort = ubah(data)
 # crpPort = UCRP(data)
 # cornReturns = np.loadtxt("./Data Sets/CORNK/" + name + "DAYCORNRETURNS.txt")
@@ -43,16 +47,27 @@ dricornReturns = np.loadtxt("./Data Sets/DRICORNK/" + name + "DAYDRICORNRETURNS.
 
 
 
-numDays = 600
+numDays = len(bestStock)
+
+# specialShowCase50 = np.loadtxt("./bullShowcaseJse.txt")
+# specialShowCase20 = np.loadtxt("./bullShowcaseJseTrainSize20.txt")
+specialShowCase10 = np.loadtxt("./bullShowcaseJseTrainSize10.txt")
+numDays = len(specialShowCase10)
+if len(crpPort) < numDays:
+    numDays = len(crpPort)
 
 plt.title("Comparison of Strategies")
 plt.xlabel("Number of Days")
 plt.ylabel("Total Return")
-# plt.plot(bestStock[0:numDays], label="Best Stock")
+plt.plot(bestStock[0:numDays], label=stockName)
 plt.plot(ubahPort[0:numDays], label="UBAH")
 plt.plot(crpPort[0:numDays], label="CRP")
-plt.plot(cornReturns, label="CORN")
-plt.plot(racornReturns, label="RACORN")
-plt.plot(dricornReturns, label="DRICORN")
+plt.plot(specialShowCase10[1:numDays], label="WEIRDK-10")
+# plt.plot(specialShowCase20[0:numDays], label="WEIRDK-20")
+# plt.plot(specialShowCase50[0:numDays], label="WEIRDK-50")
+# plt.plot(cornReturns, label="CORN")
+# plt.plot(racornReturns, label="RACORN")
+# plt.plot(dricornReturns, label="DRICORN")
+# plt.plot(weirdkReturns, label="Capital Gains Bot")
 plt.legend()
 plt.show()
