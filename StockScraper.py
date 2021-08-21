c289c94b8feb1ddd993a30f1825545c05afdc465
Jaredrhd <jaredrhd@gmail.com>
2021-08-21 15:38:22 +0200

    Updated StockScraper
    - Made compression work better
    - Forced loading of cache if cleaning of data wanted
    - Calculate Price Relatives
    
    TODO Save price relatives, load cleaned data and price relatives when loading cache

--- StockScraper.py
+++ StockScraper.py
@@ -13,8 +13,15 @@ class StockDownloader:
         self.topStocksData = None
 
         self.cachedDirectory = "StockData"
-        self.cachedFile = "stocks.zip"
-        self.fullPath = "{0}/{1}".format(self.cachedDirectory, self.cachedFile)
+        self.cachedFile = "stocks"
+        self.cleanedCachedFile = "cleaned"
+        self.fullPath = "{0}/{1}.zip".format(self.cachedDirectory, self.cachedFile)
+
+        self.compressionConfig = {'method' : 'zip', 'archive_name' : self.cachedFile + ".csv"}
+
+        self.tryCache = tryCache
+
+        self.cleanedNumpyData = None
 
         #Tries to see if we have downloaded data
         if tryCache:
@@ -25,13 +32,18 @@ class StockDownloader:
         
 
     def LoadCachedData(self):
+
+        if(self.tryCache == False):
+            print("Not allowed to load cache")
+            return False
+
         #If not a directory, create it
         if (not os.path.isdir(self.cachedDirectory)):
             os.makedirs(self.cachedDirectory)
             return False
         else:
             if(os.path.isfile(self.fullPath)):
-                self.topStocksData = pd.read_csv(self.fullPath, compression='gzip')
+                self.topStocksData = pd.read_csv(self.fullPath, dtype=object)
                 return True
         return False
 
@@ -65,11 +77,9 @@ class StockDownloader:
 
         self.topStocksData = pdr.get_data_yahoo(self.topStocksIDs, start, end)
 
-        #self.topStocksData.drop(self.topStocksData.columns[40:])
-
         #Cache it if possible
         if cacheFile:
-            self.topStocksData.to_csv(self.fullPath, compression='gzip')
+            self.topStocksData.to_csv(self.fullPath, compression=self.compressionConfig)
 
         return self
 
@@ -78,23 +88,49 @@ class StockDownloader:
 
     def CleanData(self, thresholdRowAmount = None):
 
-        #Read data
+        assert self.LoadCachedData(), "Can only clean data on cache loaded data"
+
+        #Data loaded from csv is different from downloaded data
         data = self.topStocksData.to_numpy()
-        symbols = np.unique(data[0,1:])
+        #Read data
+        data = np.delete(data, 1, 0)
+        symbols = data[0,0:41]
         dates = data[1:,0]
-        data = data[1:,1:].astype(np.float)
+        data = data[1:,1:41].astype(np.float)
         
         #Find the max non nan number row
         row = np.where(np.isnan(data))[0]
 
+        #Determine if we should threshold to keep more data
         if thresholdRowAmount is not None:
             row = row[row <= thresholdRowAmount]
         
+        #Extract data
         row = np.max(row) + 1
-        data = data[row:, :]
-        b=2
+        data = data[row:, :].astype(np.object)
+        dates = dates[row:]
 
+        self.cleanedNumpyData = data.copy()
 
+        #Insert Dates
+        data = np.insert(data, 0, dates, axis=1)
 
-a = StockDownloader(tryCache=False)
+        #Insert symbols
+        symbols[0]="Date"
+        data = np.insert(data, 0, symbols, axis=0)
+
+        #Rebuild pandas to save
+        df = pd.DataFrame(data)
+        df.to_csv("{0}/{1}_{2}.zip".format(self.cachedDirectory, self.cleanedCachedFile, self.cachedFile), compression=self.compressionConfig)
+
+        self.CalculatePriceRelative()
+
+    def CalculatePriceRelative(self):
+        data = self.cleanedNumpyData
+
+        priceRelative = data[1:,:] / data[:-1,:]
+        priceRelative= np.insert(priceRelative, 0, np.ones((1,priceRelative.shape[1])), axis=0)
+        self.priceRelative = priceRelative
+
+a = StockDownloader(tryCache=True)
 a.CleanData()
