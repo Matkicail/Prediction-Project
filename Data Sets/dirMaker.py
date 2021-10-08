d1e03039a5a0ba1cdb125c1ad33c586d81519121
Matkicail <61425800+Matkicail@users.noreply.github.com>
2021-10-08 11:36:05 +0200

    Deleted Data Section
    Done to leave only the code and methods used

--- Data Sets/dirMaker.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-names = ["BIS", "BOV", "EUR", "JSE", "NAS", "SP5"]
-for i in names:
-    os.makedirs("./WEIRDK/{0}/Testing".format(i))
-    os.makedirs("./WEIRDK/{0}/TrainVal".format(i))
-
